ae85c29324dd9c4c5af1c80d192197cc9cd1f250
goreleaserbot <goreleaser@carlosbecker.com>
2020-09-15 17:05:47 +0900

    Brew formula update for tparallel version v0.2.0

--- tparallel.rb
+++ tparallel.rb
@@ -2,16 +2,16 @@
 class Tparallel < Formula
   desc "tparallel detects inappropriate usage of t.Parallel() method in your Go test codes"
   homepage "https://github.com/moricho/tparallel"
-  version "0.1.0"
+  version "0.2.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/moricho/tparallel/releases/download/v0.1.0/tparallel_darwin_x86_64.tar.gz"
-    sha256 "ba01a0af355da642a06cfb5e1efa49369a918351b8c4a51952ed9949b5fe1a02"
+    url "https://github.com/moricho/tparallel/releases/download/v0.2.0/tparallel_darwin_x86_64.tar.gz"
+    sha256 "ec7fe8bcef33fe1903840213fadada382b6e210ce937a2c608942d7bffa5caac"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/moricho/tparallel/releases/download/v0.1.0/tparallel_linux_x86_64.tar.gz"
-      sha256 "d233696c70ecb58b0760baf720299509ef776eba58ff55f1b102bdb156746f59"
+      url "https://github.com/moricho/tparallel/releases/download/v0.2.0/tparallel_linux_x86_64.tar.gz"
+      sha256 "1a8ccb43d95221b07a696f096d778682453ab80093502c345c0598fba13a767c"
     end
   end
 
